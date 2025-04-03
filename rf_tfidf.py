7555a71764515a1ebd0fa00cee8bbf23a10dec76
aquablue07 <50393476+aquablue07@users.noreply.github.com>
2025-04-02 19:07:19 -0500

    Update rf_tfidf.py

--- rf_tfidf.py
+++ rf_tfidf.py
@@ -11,14 +11,14 @@ from sklearn.model_selection import train_test_split, cross_val_score, KFold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 
-# Load your data
+# Load data
 df = pd.read_csv('/Users/vishwa/final_spacy.csv')
 
 numerical_cols = ['Price', 'Weight_lbs', 'Volume']
 categorical_cols = ['Manufacturer', 'SKU']
 text_col = 'Processed_Text'
 
-# NaN Handling
+# Nan handling
 num_imputer = SimpleImputer(strategy='median')
 df[numerical_cols] = num_imputer.fit_transform(df[numerical_cols])
 df[categorical_cols] = df[categorical_cols].fillna('missing')
@@ -33,7 +33,7 @@ X = df.drop('Category', axis=1)
 y = df['Category']
 
 # Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, shuffle=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=7, shuffle=True)
 
 # Preprocessing Pipeline
 preprocessor = ColumnTransformer(
@@ -52,8 +52,8 @@ preprocessor = ColumnTransformer(
 # Model Pipeline
 model = Pipeline([
     ('preprocessor', preprocessor),
-    ('classifier', RandomForestClassifier(random_state=42, n_estimators=400, class_weight='balanced',
-                                           max_depth=100, min_samples_split=2, min_samples_leaf=2)) #added regularization
+    ('classifier', RandomForestClassifier(random_state=7, n_estimators=400, class_weight='balanced',
+                                           max_depth=10, min_samples_split=2, min_samples_leaf=2)) #added regularization
 ])
 
 # Train model
